644ec192bf85b735934e93eb616ece59fd5e0d24
rupamShaw1998 <rupamshw77777@gmail.com>
2022-03-04 01:04:32 +0530

    first commit

---
+++ 1.Calculator/main.js
@@ -0,0 +1,9 @@
+const add = require("./add.js");
+const sub = require("./subtract.js");
+const mul = require("./multiply.js");
+const div = require("./divide.js");
+
+console.log("addition:", add(9,4));
+console.log("subtraction:", sub(5,2));
+console.log("multiplication:", mul(6,7));
+console.log("division:", div(28,4));
