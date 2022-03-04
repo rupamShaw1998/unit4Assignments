989fc907b1e384fa8ca326fa2667bed5c94fd00d
rupamShaw1998 <rupamshw77777@gmail.com>
2022-03-04 21:47:46 +0530

    day 2 assignment added

---
+++ 2.Express/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/home", function (req, res) {
+    res.send("Hello");
+})
+
+app.get("/books", function (req, res) {
+    res.send({
+        book1: "Harry Potter",
+        book2: "Time Management",
+        book3: "Malgudi Days"
+    })
+})
+
+app.listen(5000, ()=>{
+    console.log("listening at port 5000");
+})
--- test.js
+++
@@ -1,2 +0,0 @@
-console.log("rs");
-console.log("india");
