d804602b8c9d14fb379341b31cdb3675dc9382fa
rupamShaw1998 <rupamshw77777@gmail.com>
2022-03-25 00:51:41 +0530

    relationship updated

--- 4.Relationship/src/index.js
+++ 4.Relationship/src/index.js
@@ -4,6 +4,17 @@ const app =express();
 app.use(express.json());
 
 const bookController = require("./controllers/book.controllers");
+const authorController = require("./controllers/author.controllers");
+const authorBookController = require("./controllers/authorBook.controllers");
+const checkOutController = require("./controllers/checkOut.controllers");
+const sectionController = require("./controllers/section.controllers");
+const userController = require("./controllers/user.controllers");
+
 app.use("/books", bookController);
+app.use("/books", authorController);
+app.use("/books", authorBookController);
+app.use("/books", checkOutController);
+app.use("/books", sectionController);
+app.use("/books", userController);
 
 module.exports = app;
